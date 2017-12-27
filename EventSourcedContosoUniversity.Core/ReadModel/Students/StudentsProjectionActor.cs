ac9d10a9970dce2eb54a0854f32ebe0f0030a68d
Hossam Barakat <hossam.eldeen@gmail.com>
2017-12-27 19:26:43 +1100

    Update StudentsProjectionActor to use BaseProjectionActor

--- EventSourcedContosoUniversity.Core/ReadModel/Students/StudentsProjectionActor.cs
+++ EventSourcedContosoUniversity.Core/ReadModel/Students/StudentsProjectionActor.cs
@@ -1,27 +1,19 @@
-using System;
-using Akka.Actor;
 using EventSourcedContosoUniversity.Core.Domain.Events;
 using EventSourcedContosoUniversity.Core.Infrastructure.EventStore;
 using EventSourcedContosoUniversity.Core.ReadModel.Repositories;
 using EventSourcedContosoUniversity.Core.ReadModel.Students;
-using EventStore.ClientAPI;
 
 namespace EventSourcedContosoUniversity.Core.ReadModel.Departments
 {
-    public class StudentsProjectionActor : ReceiveActor
+    public class StudentsProjectionActor : BaseProjectionActor<StudentReadModel>
     {
-        readonly EventStoreDispatcher _dispatcher;
         private readonly ICatchupPositionRepository _catchupPositionRepository;
 
-        public class SaveEventMessage
-        {
-            public long CommitPosition { get; set; }
-            public long PreparePosition { get; set; }
-        }
         public StudentsProjectionActor(EventStoreDispatcher dispatcher, IReadModelRepository repository, ICatchupPositionRepository catchupPositionRepository)
+            : base(dispatcher,repository,catchupPositionRepository)
         {
             _catchupPositionRepository = catchupPositionRepository;
-            _dispatcher = dispatcher;
+            
             ReceiveAsync<StudentCreated>(async (s) =>
             {
                 await repository.Add(new StudentReadModel
@@ -46,35 +38,7 @@ namespace EventSourcedContosoUniversity.Core.ReadModel.Departments
                 var student = await repository.GetById<StudentReadModel>(s.Id);
                 await repository.Delete(student);
             });
-            ReceiveAsync<SaveEventMessage>( async (s) =>
-            {
-                await _catchupPositionRepository.SavePosition<StudentReadModel>(new Position(s.CommitPosition, s.PreparePosition));
-            });
         }
 
-        protected override void PreStart()
-        {
-            var sender = Self;
-            var last= _catchupPositionRepository.GetLastProcessedPosition<StudentReadModel>().Result ;
-            var lastProcessedPosition = last != null ? new Position(last.CommitPosition, last.PreparePosition) : Position.Start;
-            
-            _dispatcher.StartDispatchingForAll(lastProcessedPosition.CommitPosition, lastProcessedPosition.PreparePosition, (o) =>
-            {
-                sender.Tell(o);
-            },
-                (commitPosition, preparePosition) =>
-                {
-                    sender.Tell(new SaveEventMessage
-                    {
-                        CommitPosition = commitPosition,
-                        PreparePosition = preparePosition
-                    });
-                });
-        }
-
-        protected override void PostStop()
-        {
-            _dispatcher.StopDispatching(TimeSpan.FromSeconds(3000));
-        }
     }
 }
